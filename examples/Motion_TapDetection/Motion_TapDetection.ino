43722dbf27179bb2b703c8fa619133789a6e3cbe
microbots <info@microbots.io>
2024-09-11 16:11:57 +0200

    Update Motion_TapDetection.ino

--- examples/Motion_TapDetection/Motion_TapDetection.ino
+++ examples/Motion_TapDetection/Motion_TapDetection.ino
@@ -1,8 +1,9 @@
 /*
  * Overview:
  * This code demonstrates the CodeCell's Tap Detection feature.
- * In this example, the CodeCell continuously searches for a tap. 
- * When one is detected, it shines the LED yellow for 1 second.
+ * In this example, the CodeCell continuously searches for a tap. When one is detected, it shines the LED yellow for 1 second.
+ * Feel free to tweak the code with your own creative ideas! 
+ * Learn more about this example here - https://microbots.io/blogs/learn/codecell-tap-detection
  */
 
 #include <CodeCell.h>
@@ -10,18 +11,17 @@
 CodeCell myCodeCell;
 
 void setup() {
-  Serial.begin(115200); /* Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial. */
-  
-  myCodeCell.Init(MOTION_TAP_DETECTOR); /*Initializes Tap Detection Sensing*/
+    Serial.begin(115200); // Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial
+    myCodeCell.Init(MOTION_TAP_DETECTOR); // Initializes tap detection sensing
 }
 
 void loop() {
-  if (myCodeCell.Run()) {
-    /*Runs  every 100ms*/
-      if(myCodeCell.Motion_TapRead()){
-        /*If Tap is detected shine the LED Yellow for 1 sec*/
-        myCodeCell.LED(0XA0, 0x60, 0x00U);
-        delay(1000);
-      }
-  }
+    if (myCodeCell.Run()) {
+        // Runs every 100ms to check for taps
+        if (myCodeCell.Motion_TapRead()) {
+            // If a tap is detected, shine the LED yellow for 1 second
+            myCodeCell.LED(0xA0, 0x60, 0x00); // Set LED to yellow
+            delay(1000); // Keep the LED on for 1 second
+        }
+    }
 }
