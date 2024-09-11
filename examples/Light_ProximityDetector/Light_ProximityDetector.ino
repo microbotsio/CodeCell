d1ddffb6a552a5b9432248cdf247c08bfbebc862
microbots <info@microbots.io>
2024-09-11 15:40:24 +0200

    Update Light_ProximityDetector.ino

--- examples/Light_ProximityDetector/Light_ProximityDetector.ino
+++ examples/Light_ProximityDetector/Light_ProximityDetector.ino
@@ -1,21 +1,30 @@
+/*
+ * Overview:
+ * This code demonstrates the CodeCell's prximity sensing.
+ * In this example, the CodeCell continuously measures proximity and set the onboard LED to Red when proximity is detected
+ * Feel free to tweak the code with your own creative ideas! 
+ * Learn more about this example here - https://microbots.io/blogs/learn/codecell-proximity-sensing
+ */
+
 #include <CodeCell.h>
 CodeCell myCodeCell;
 
 void setup() {
-  Serial.begin(115200); /* Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial. */
-
-  myCodeCell.Init(LIGHT); /*Initializes Light Sensing*/
+    Serial.begin(115200); // Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial
+    myCodeCell.Init(LIGHT); // Initializes light sensing, including proximity
 }
 
 void loop() {
-  if (myCodeCell.Run()) {
-    /*Runs  every 100ms*/
-    uint16_t proximity = myCodeCell.Light_ProximityRead();
-    if (proximity > 100) {
-      myCodeCell.LED(0xFF, 0, 0); /*Set to Red*/
-      delay(1000);
-    } else {
-      /*Skip*/
+    if (myCodeCell.Run()) {
+        // Runs every 100ms to check proximity
+        uint16_t proximity = myCodeCell.Light_ProximityRead();
+        
+        // Check if an object is within range
+        if (proximity > 100) {
+            myCodeCell.LED(0xFF, 0, 0); // Set LED to Red when proximity is detected
+            delay(1000); // Keep the LED on for 1 second
+        } else {
+            // No action if the object is out of range
+        }
     }
-  }
 }
