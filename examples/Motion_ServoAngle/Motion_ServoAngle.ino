ed2efbbb63fbb2864c1f1474e3c71ec9074b1e39
microbots <info@microbots.io>
2024-09-11 16:56:51 +0200

    Update Motion_ServoAngle.ino

--- examples/Motion_ServoAngle/Motion_ServoAngle.ino
+++ examples/Motion_ServoAngle/Motion_ServoAngle.ino
@@ -1,16 +1,16 @@
 /*
  * Overview:
  * This code demonstrates the CodeCell's Rotation Sensing features.
- * In this example, the CodeCell continuously reads the angular rotations 
- * Every 100ms it prints the Roll, Pitch, and Yaw angles on the Serial Monitor
+ * In this example, the CodeCell continuously reads the angular rotations and use these angles to control a servo motor
  * Feel free to tweak the code with your own creative ideas!
+ * Learn more about this example here - https://microbots.io/blogs/learn/codecell-servo-angle-control
  */
 
 #include <CodeCell.h>
 #include <ESP32Servo.h>
 
 CodeCell myCodeCell;
-Servo myservo;  // create servo object to control a servo
+Servo myservo;  
 
 float Roll = 0.0;
 float Pitch = 0.0;
@@ -18,29 +18,28 @@ float Yaw = 0.0;
 int servo_angle = 0;
 
 void setup() {
-  Serial.begin(115200); /* Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial. */
-
-  myCodeCell.Init(MOTION_ROTATION); /*Initializes Rotation Sensing*/
-  myservo.attach(1);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(115200); // Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial
+    myCodeCell.Init(MOTION_ROTATION); // Initializes rotation sensing
+    myservo.attach(1);  // Attaches the servo on pin 1 to the servo object
 }
 
 void loop() {
-  if (myCodeCell.Run()) {
-    myCodeCell.Motion_RotationRead(Roll, Pitch, Yaw);
-    servo_angle = abs((int)Pitch);
-    servo_angle = (180 - servo_angle);
-
-    if(servo_angle> 60){
-      servo_angle = 60;
+    if (myCodeCell.Run()) {
+        // Read rotation angles from the BNO085 sensor
+        myCodeCell.Motion_RotationRead(Roll, Pitch, Yaw);
+        
+        // Convert the pitch angle to a servo angle
+        servo_angle = abs((int)Pitch);
+        servo_angle = (180 - servo_angle);
+        
+        // Limit the servo angle to the range 0-60 degrees
+        if (servo_angle > 60) {
+            servo_angle = 60;
+        } else if (servo_angle < 0) {
+            servo_angle = 0;
+        }
+        
+        Serial.println(servo_angle); // Print the servo angle for debugging
+        myservo.write(servo_angle);  // Set the servo position
     }
-    else if (servo_angle < 0){
-      servo_angle = 0;
-    }
-    else{
-      /*skip*/
-    }
-
-    Serial.println(servo_angle);
-    myservo.write(servo_angle);
-  }
 }
