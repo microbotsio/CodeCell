0d7e4f3e32ea620e373bf447b3290c4d20db7d42
microbots <info@microbots.io>
2025-01-24 15:44:58 +0100

    Update Motion_PersonalActivity.ino

--- examples/Motion_PersonalActivity/Motion_PersonalActivity.ino
+++ examples/Motion_PersonalActivity/Motion_PersonalActivity.ino
@@ -41,7 +41,7 @@ void setup() {
 }
 
 void loop() {
-  if (myCodeCell.Run()) {
+  if (myCodeCell.Run(10)) {  /*Run every 100ms (10Hz)*/
     /*Runs  every 100ms*/
     if (read_timer < 10) {
       read_timer++;
