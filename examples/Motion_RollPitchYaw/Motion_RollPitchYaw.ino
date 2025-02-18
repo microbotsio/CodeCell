39713ff4027822ad5f86b3c9e36d1176ffd6ff40
microbots <info@microbots.io>
2025-02-18 17:58:29 +0100

    Update Motion_RollPitchYaw.ino

--- examples/Motion_RollPitchYaw/Motion_RollPitchYaw.ino
+++ examples/Motion_RollPitchYaw/Motion_RollPitchYaw.ino
@@ -15,13 +15,13 @@ float Pitch = 0.0;
 float Yaw = 0.0;
 
 void setup() {
-  Serial.begin(115200); /* Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial. */
+  Serial.begin(115200); // Set Serial baud rate to 115200. Ensure Tools/USB_CDC_On_Boot is enabled if using Serial.
 
-  myCodeCell.Init(MOTION_ROTATION); /*Initializes Rotation Sensing*/
+  myCodeCell.Init(MOTION_ROTATION); //Initializes Rotation Sensing
 }
 
 void loop() {
-  if (myCodeCell.Run(10)) {  /*Run every 100ms (10Hz)*/
+  if (myCodeCell.Run(10)) {  //Run every 10Hz
     myCodeCell.Motion_RotationRead(Roll, Pitch, Yaw);
     Serial.printf("Roll: %.2f°, Pitch: %.2f°, Yaw: %.2f°\n", Roll, Pitch, Yaw);
   }
