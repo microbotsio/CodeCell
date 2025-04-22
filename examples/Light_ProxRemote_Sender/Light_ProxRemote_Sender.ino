1e602903461705b68d6406f2a474edf060e6dda2
microbots <info@microbots.io>
2025-04-22 14:41:16 +0200

    Update Light_ProxRemote_Sender.ino

--- examples/Light_ProxRemote_Sender/Light_ProxRemote_Sender.ino
+++ examples/Light_ProxRemote_Sender/Light_ProxRemote_Sender.ino
@@ -3,7 +3,7 @@
  * This code demonstrates how to configure the CodeCell's ESP32-C3 to be used as a WiFi remote
  * In this example, the CodeCell (Device 1) reads the proximity sensor and sends it to Device 2
  * Feel free to tweak the code with your own creative ideas!
- * Learn more about this example here - https://microbots.io/blogs/learn/codecell-wifi-remote
+ * Learn more about this example here - https://microbots.io/blogs/codecell/codecell-wifi-remote
  */
 
 #include <esp_now.h>
